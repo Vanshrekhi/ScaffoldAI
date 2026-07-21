a1d47a73844ab1758664c05b6b23378f8389a880
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-21 17:41:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"https://scaffoldai-1-client.onrender.com",
+    origin: "https://interviewiq-ai-frontend-client.onrender.com",
     credentials:true
 }))
 
