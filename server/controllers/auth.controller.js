0a938582e1681f23e848259b36b8ce15dd3a7654
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-21 17:17:39 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,33 +1,23 @@
 import genToken from "../config/token.js"
 import User from "../models/user.model.js"
 
+export const googleAuth = async (req, res) => {
+  try {
+    const { name, email } = req.body
+    let user = await User.findOne({ email })
+    if (!user) user = await User.create({ name, email })
 
-export const googleAuth = async (req,res) => {
-    try {
-        const {name , email} = req.body
-        let user = await User.findOne({email})
-        if(!user){
-            user = await User.create({
-                name , 
-                email
-            })
-        }
-        let token = await genToken(user._id)
-       return res.cookie("token", token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "None",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-});
-        
-        return res.status(200).json(user)
+    let token = await genToken(user._id)
 
-
-
-    } catch (error) {
-        return res.status(500).json({message:`Google auth error ${error}`})
-    }
-    
+    return res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token   // ← ye add karna zaroori hai
+    })
+  } catch (error) {
+    return res.status(500).json({ message: `Google auth error ${error}` })
+  }
 }
 
 export const logOut = async (req,res) => {
