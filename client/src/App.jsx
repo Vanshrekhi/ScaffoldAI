4c5ce6587b2b4f818f92746b614a8ff0c53487c0
Vansh Rekhi <vanshrekhi05@gmail.com>
2026-07-21 17:35:54 +0530

    Update ServerUrl to new backend endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://scaffoldai.onrender.com"
+export const ServerUrl  = "https://interviewiq-ai-backend-server.onrender.com"
 
 axios.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
